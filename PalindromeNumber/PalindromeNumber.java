7b6d84e9ee3f8869d331af58436cae6d3d5b5e94
nikhilarora <nikhil290192@gmail.com>
2020-07-22 19:51:07 +0530

    PalindromeNumber

---
+++ PalindromeNumber/PalindromeNumber.java
@@ -0,0 +1,22 @@
+
+
+class PalindromeNumber {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(1221)); 
+    }
+
+    public static boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int revertedNumber = 0;
+        while(x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+
+        return x == revertedNumber || x == revertedNumber/10;
+    }
+}
--- ReverseInteger/ReverseInteger.java
+++ ReverseInteger/ReverseInteger.java
@@ -1,9 +1,17 @@
 
-
 class ReverseInteger {
     public static void main(String[] args) {
-        System.out.println(reverse(123));
+        System.out.println(reverse(1234));
     }
+
+
+    /*
+    Complexity Analysis:-
+    Time Complexity: log10(x). There are roughly log10(x) digits in x.
+
+    Space Complexity: O(1)
+
+    */
 
     public static int reverse(int x) {
         int rev = 0;
@@ -17,10 +25,8 @@ class ReverseInteger {
             if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) {
                 return 0;
             }
-
             rev = rev * 10 + pop;
         }
-
         return rev;
     }
 }
