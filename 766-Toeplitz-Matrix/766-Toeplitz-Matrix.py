a5b0dfdb31a26c7612a744455d84add40d879a7f
Nikhil Arora <nikhil290192@gmail.com>
2021-07-30 23:15:42 +0530

    766 completed

---
+++ 766-Toeplitz-Matrix/766-Toeplitz-Matrix.py
@@ -0,0 +1,15 @@
+# 766. Toeplitz Matrix
+# https://leetcode.com/problems/toeplitz-matrix/
+
+class Solution(object):
+    def isToeplitzMatrix(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: bool
+        """
+        m, n = len(matrix), len(matrix[0])
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][j] != matrix[i - 1][j - 1]:
+                    return False
+        return True
