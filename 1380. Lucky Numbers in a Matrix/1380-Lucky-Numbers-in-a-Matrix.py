b713371494c2b5acc97ba1980ecdf6266aac6e30
Nikhil Arora <nikhil290192@gmail.com>
2021-08-01 21:54:34 +0530

    This week Problems.

---
+++ 1380. Lucky Numbers in a Matrix/1380-Lucky-Numbers-in-a-Matrix.py
@@ -0,0 +1,2 @@
+# 1380. Lucky Numbers in a Matrix
+# https://leetcode.com/problems/lucky-numbers-in-a-matrix/
---
+++ 674. Longest Continuous Increasing Subsequence/674-Longest-Continuous-Increasing-Subsequence.py
@@ -0,0 +1,2 @@
+# 674. Longest Continuous Increasing Subsequence
+# https://leetcode.com/problems/longest-continuous-increasing-subsequence/
