206435768f151deae0312bec5cfc5aa48c2513f0
Nikhil Arora <nikhil290192@gmail.com>
2021-08-02 23:39:15 +0530

    566 completd

--- 566. Reshape the Matrix/566-Reshape-the-Matrix.py
+++ 566. Reshape the Matrix/566-Reshape-the-Matrix.py
@@ -1,2 +1,24 @@
 # 566. Reshape the Matrix
 # https://leetcode.com/problems/reshape-the-matrix/
+
+class Solution(object):
+    def matrixReshape(self, nums, r, c):
+        """
+        :type nums: List[List[int]]
+        : type r: int
+        : type c: int
+        :rtype: List[List[int]]
+        """
+        m, n = len(nums), len(nums[0])
+        if m * n != r * c:
+            return nums
+        ans = [[0 for _ in range(c)] for _ in range(r)]
+        p1, p2 = 0, 0
+        for i in range(m):
+            for j in range(n):
+                ans[p1][p2] = nums[i][j]
+                p2 += 1
+                if p2 == c:
+                    p1 += 1
+                    p2 = 0
+        return ans
