ca55ca378283efd7a166d7311fa61d12392887a9
Nikhil Arora <nikhil290192@gmail.com>
2021-08-08 22:57:40 +0530

    674 completed

--- 674. Longest Continuous Increasing Subsequence/674-Longest-Continuous-Increasing-Subsequence.py
+++ 674. Longest Continuous Increasing Subsequence/674-Longest-Continuous-Increasing-Subsequence.py
@@ -1,2 +1,18 @@
 # 674. Longest Continuous Increasing Subsequence
 # https://leetcode.com/problems/longest-continuous-increasing-subsequence/
+
+
+class Solution(object):
+    def findLengthOfLCIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 0:
+            return 0
+        dp = [0] * len(nums)
+        for i in range(len(nums)):
+            dp[i] = 1
+            if i > 0 and nums[i] > nums[ i - 1 ]:
+                dp[i] = max(dp[i], dp[i - 1] + 1)
+        return max(dp)
