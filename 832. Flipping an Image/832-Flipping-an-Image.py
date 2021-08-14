9f5c7f97af4f330ddf2d1b9dcb1cb177685840e3
Nikhil Arora <nikhil290192@gmail.com>
2021-08-14 10:49:04 +0530

    832 completed

--- 832. Flipping an Image/832-Flipping-an-Image.py
+++ 832. Flipping an Image/832-Flipping-an-Image.py
@@ -1,2 +1,17 @@
 # 832. Flipping an Image
 # https://leetcode.com/problems/flipping-an-image/
+
+class Solution(object):
+    def flipAndInvertImage(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        x, y = len(A), len(A[0])
+        for i in range(x):
+            A[i] = A[i][::-1]
+            for j in range(y):
+                A[i][j] ^= 1
+        return A
+
+
